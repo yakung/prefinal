c2ce19d018838ff071bcff56fca673965c73ffb9
yakung <60070007@kmitl.ac.th>
2019-05-25 02:59:32 +0700

    file

---
+++ lib/ui/Textfile.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+class Textfile{
+   Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.txt');
+  }
+
+  Future<File> writeCounter(int counter) async {
+    final file = await _localFile;
+    return file.writeAsString('$counter');
+  }
+
+  Future<int> readCounter() async {
+    try {
+      final file = await _localFile;
+      String contents = await file.readAsString();
+      return int.parse(contents);
+    } catch (e) {
+      return 0;
+    }
+  }
+}
